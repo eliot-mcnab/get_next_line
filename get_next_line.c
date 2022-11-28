dd5015921270c787bb5492777d928434fa1a7c75
emcnab <eliot.code@proton.me>
2022-11-28 12:03:15 +0100

    docs: added documentation to get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: emcnab <emcnab@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 17:45:32 by emcnab            #+#    #+#             */
-/*   Updated: 2022/11/28 11:49:45 by emcnab           ###   ########.fr       */
+/*   Updated: 2022/11/28 12:02:49 by emcnab           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,17 @@
 #include "get_next_line_bonus.h"
 #include <stdlib.h>
 
+/*
+ * @brief Gets the next line in the file corresponding to the given file 
+ * 	directory [fd]. Starts at the beggining of the file and returns the 
+ * 	following line on each successive function call. Currently only handles one
+ * 	file at a time.
+ *
+ * @param fd (int): file directory of the file to read. Must point to a file
+ * 	wich was opened with read permissions enabled.
+ *
+ * @return (char *): the next line in the file corresponding to [fd].
+ */
 char	*get_next_line(int fd)
 {
 	static t_line	line = {0, };
@@ -42,6 +53,14 @@ char	*get_next_line(int fd)
 	return (line_current);
 }
 
+/*
+ * @brief Strign memory allocation. Allocates size + 1 bytes of memory for a a
+ * 	string and null-terminates the result, leaving size bytes to be written.
+ *
+ * @param size (size_t): the amount of characters to allocate space for.
+ *
+ * @return (char *): string with space allocated for [size] characters.
+ */
 char	*ft_stralloc(size_t size)
 {
 	char	*str;
@@ -53,6 +72,18 @@ char	*ft_stralloc(size_t size)
 	return (str);
 }
 
+/*
+ * @brief Copies the contents of a string between [str_start] and [str_end].
+ * 	If [str_end] [str_start] then no bytes will be copied. At most [str_end] - 
+ * 	[str_start] bytes will be copied, but if the terminating null byte in 
+ * 	[str_start] is before [str_end], copy will stop there.
+ *
+ * @param str_start (char *): the start of the string to copy.
+ * @param str_end (char *): the end of the string to copy. 
+ *
+ * @return (char *): the substring in [str_start] between [str_start] and
+ * 	[str_end].
+ */
 char	*ft_substr(char *str_start, char *str_end)
 {
 	char	*strcpy;
@@ -64,7 +95,7 @@ char	*ft_substr(char *str_start, char *str_end)
 	if (!strcpy)
 		return (NULL);
 	i = 0;
-	while (str_start + i < str_end)
+	while (str_start[i] && str_start + i < str_end)
 	{
 		strcpy[i] = str_start[i];
 		i++;
